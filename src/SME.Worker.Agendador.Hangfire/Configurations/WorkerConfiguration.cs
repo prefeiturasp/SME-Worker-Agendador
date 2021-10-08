ebd775563fbd15ce11ea296f821ac5caa0a2e1ce
marcoslobo <marcos.lobo.nit@gmail.com>
2021-10-08 19:15:53 -0300

    Ajustando porta do redis

--- src/SME.Worker.Agendador.Hangfire/Configurations/WorkerConfiguration.cs
+++ src/SME.Worker.Agendador.Hangfire/Configurations/WorkerConfiguration.cs
@@ -1,6 +1,8 @@
 ﻿using Hangfire;
 using Microsoft.Extensions.DependencyInjection;
 using SME.Worker.Agendador.Infra.Utilitarios;
+using Hangfire.Redis;
+
 
 namespace SME.Worker.Agendador.Hangfire.Configurations
 {
@@ -8,12 +10,14 @@ namespace SME.Worker.Agendador.Hangfire.Configurations
     {
         public static void Configure(IServiceCollection services, ConfiguracaoHangfireOptions configuracaoHangfireOptions)
         {
+            var options = new RedisStorageOptions(){ Db = configuracaoHangfireOptions.RedisDbNumber };
+
             services.AddHangfire(configuration => configuration
             .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
             .UseSimpleAssemblyNameTypeSerializer()
             .UseRecommendedSerializerSettings()
             .UseFilter<AutomaticRetryAttribute>(new AutomaticRetryAttribute() { Attempts = 0 })
-            .UseRedisStorage(configuracaoHangfireOptions.ConnectionString));
+            .UseRedisStorage(configuracaoHangfireOptions.ConnectionString, options));
         }
     }
 }
