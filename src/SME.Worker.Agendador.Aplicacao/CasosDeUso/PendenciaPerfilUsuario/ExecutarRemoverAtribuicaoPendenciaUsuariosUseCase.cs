005c7693cdda4baa2b9c60aedc395f107abea774
Vinícius Nyari <vinicius.nyari@amcom.com.br>
2021-11-19 10:00:44 -0300

    Adicionar Remover Atribuição de Pendência Usuario ao worker

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/PendenciaPerfilUsuario/ExecutarRemoverAtribuicaoPendenciaUsuariosUseCase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Sentry;
+using SME.Worker.Agendador.Aplicacao.Comandos;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.ConsolidacaoRegistrosPedagogicos
+{
+    public class ExecutarRemoverAtribuicaoPendenciaUsuariosUseCase : AbstractUseCase, IExecutarRemoverAtribuicaoPendenciaUsuariosUseCase
+    {
+        public ExecutarRemoverAtribuicaoPendenciaUsuariosUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+        public async Task Executar()
+        {
+            SentrySdk.AddBreadcrumb($"Mensagem ExecutarRemoverAtribuicaoPendenciaUsuariosUseCase", "Rabbit - ExecutarRemoverAtribuicaoPendenciaUsuariosUseCase");
+
+            await mediator.Send(new PublicarFilaSgpCommand(RotasRabbitSgp.RemoverAtribuicaoPendenciaUsuariosUseCase, string.Empty, Guid.NewGuid()));
+        }
+    }
+}
