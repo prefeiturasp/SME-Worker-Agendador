44801b6b91f934f2bada96dfbb69fd7ed0721b76
Caique Siqueira <latorrecaique@gmail.com>
2022-02-08 19:43:21 -0300

    AB#57939

--- src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSerap.cs
+++ src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSerap.cs
@@ -3,5 +3,6 @@
     public static class RotasRabbitSerap
     {
         public const string RotaSincronizacaoInstitucional = "serap.sincronizacao.institucional.dre.sync";
+        public const string ProvaSync = "serap.estudante.prova.legado.sync";
     }
 }
