5846022a0c04053fb4af9db6516102a828e196e5
Caique Siqueira <latorrecaique@gmail.com>
2022-02-10 17:39:26 -0300

    AB#57939 Agendador de Finalização de prova

--- src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSerap.cs
+++ src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSerap.cs
@@ -4,5 +4,6 @@
     {
         public const string RotaSincronizacaoInstitucional = "serap.sincronizacao.institucional.dre.sync";
         public const string ProvaSync = "serap.estudante.prova.legado.sync";
+        public const string IniciarProcessoFinalizarProvasAutomaticamente = "serap.estudante.prova.finalizar.automaticamente.iniciar";
     }
 }
