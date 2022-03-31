b4587b96b8a65ce1161313c461fd29f29e45d846
krisamcom <68442245+krisamcom@users.noreply.github.com>
2022-03-31 11:21:49 -0300

    story/57323 - (SERAP ESTUDANTES) sync usuários coresso e abrangência

--- src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSerap.cs
+++ src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSerap.cs
@@ -5,5 +5,6 @@
         public const string RotaSincronizacaoInstitucional = "serap.sincronizacao.institucional.dre.sync";
         public const string ProvaSync = "serap.estudante.prova.legado.sync";
         public const string IniciarProcessoFinalizarProvasAutomaticamente = "serap.estudante.prova.finalizar.automaticamente.iniciar";
+        public const string UsuarioCoreSsoSync = "serap.estudante.usuario.coresso.sync";
     }
 }
