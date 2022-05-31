84b3761c3ec5cee6e20e76e258b82c3df6b5ba53
Danilo Ferreira <shymmi@hotmail.com>
2022-05-31 17:08:49 -0300

    implementação do worker no hangfire (branch nova pois o VS mudou muito do código com as ferramentas de clean code)

--- src/SME.Worker.Agendador.IoC/RegistraDependenciasAgendador.cs
+++ src/SME.Worker.Agendador.IoC/RegistraDependenciasAgendador.cs
@@ -133,7 +133,8 @@ namespace SME.Worker.Agendador.IoC
             services.TryAddScopedWorkerService<IEncerrarEncaminhamentoAEEAutomaticoSyncUseCase, EncerrarEncaminhamentoAEEAutomaticoSyncUseCase>();
             services.TryAddScopedWorkerService<IFilaTesteRabbitMQ, FilaTesteRabbitMQ>();
             services.TryAddScopedWorkerService<IReprocessarDiarioBordoPendenciaDevolutivaUseCase, ReprocessarDiarioBordoPendenciaDevolutivaUseCase>();
-            
+            services.TryAddScopedWorkerService<IAtribuicaoResponsaveisUseCase, AtribuicaoResponsaveisUseCase>();
+
             services.TryAddScopedWorkerService<Infra.Interfaces.IContextoAplicacao, WorkerContext>();
         }
 
