9bf1dae10ae0924c71e67d9792687d8360966cd5
Rafael Losi <rafael.losi@amcom.com.br>
2022-09-15 11:16:05 -0300

    73927 Remoção do agendamento do processamento de Deadletters

--- src/SME.Worker.Agendador.Background/RegistraServicosRecorrentes.cs
+++ src/SME.Worker.Agendador.Background/RegistraServicosRecorrentes.cs
@@ -133,9 +133,6 @@ namespace SME.Worker.Agendador.Background
 
             Cliente.ExecutarPeriodicamente<IConciliacaoFrequenciaTurmasCronUseCase>(c => c.Executar(), Cron.Weekly(System.DayOfWeek.Saturday, 23));
 
-            //De 10 em 10 minutos
-            Cliente.ExecutarPeriodicamente<IRabbitDeadletterSgpSyncUseCase>(c => c.Executar(), Cron.MinuteInterval(10));
-
             Cliente.ExecutarPeriodicamente<IExecutarSincronizacaoMediaRegistrosIndividuaisSyncUseCase>(c => c.Executar(), Cron.Daily(9));
 
             // Consolidação Acompanhamento Aprendizagem do Aluno
