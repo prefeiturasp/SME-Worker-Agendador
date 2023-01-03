4371ba849f93c407fffc6f84510a7c4e6e196d28
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-01-03 11:39:33 -0300

    82426 - Criação do processo agendado

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/EncaminhamentoNAAPA/AtualizarInformacoesDoEncaminhamentoNAAPA/IAtualizarInformacoesDoEncaminhamentoNAAPA.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.EncaminhamentoNAAPA
+    public interface IAtualizarInformacoesDoEncaminhamentoNAAPA
+    {
+        Task Executar();
+    }
+}
