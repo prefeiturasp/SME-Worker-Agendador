8c9874d0dcd980fd617351ebffb6735a659ef3e1
Danyllo Pereira <danyllo.antunes@gmail.com>
2023-05-16 15:25:29 -0300

    91181 - Agendar a execução do totalizador

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/AtualizarTotalizadoresDePendencia/AtualizarTotalizadoresDePendenciaUseCase.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SME.Worker.Agendador.Aplicacao.Comandos;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.AtualizarTotalizadoresDePendencia
+{
+    public class AtualizarTotalizadoresDePendenciaUseCase : AbstractUseCase, IAtualizarTotalizadoresDePendenciaUseCase
+    {
+        public AtualizarTotalizadoresDePendenciaUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task Executar()
+        {
+            await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbitSgp.ExecutarAtualizacaoDosTotalizadoresDasPendencias, Guid.NewGuid()));
+        }
+    }
+}
