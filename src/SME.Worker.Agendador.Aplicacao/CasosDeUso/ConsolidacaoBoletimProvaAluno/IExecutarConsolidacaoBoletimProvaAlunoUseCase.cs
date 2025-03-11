0a3831711c34779006d0f1f35ea158412e5f0dcc
Mario Junior <mariosj9@gmail.com>
2025-03-11 14:21:59 -0300

    AB#125873

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/ConsolidacaoBoletimProvaAluno/IExecutarConsolidacaoBoletimProvaAlunoUseCase.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.ConsolidacaoBoletimProvaAluno
+{
+    public interface IExecutarConsolidacaoBoletimProvaAlunoUseCase
+    {
+        Task Executar();
+    }
+}
