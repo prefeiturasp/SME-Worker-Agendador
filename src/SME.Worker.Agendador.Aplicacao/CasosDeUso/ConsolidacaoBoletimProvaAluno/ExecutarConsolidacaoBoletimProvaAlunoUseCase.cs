7216b878f138020690bf6566e84634a8cb76a9a8
Mario Junior <mariosj9@gmail.com>
2025-03-12 16:31:17 -0300

    AB#125873

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/ConsolidacaoBoletimProvaAluno/ExecutarConsolidacaoBoletimProvaAlunoUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Sentry;
+using SME.Worker.Agendador.Aplicacao.Comandos.PublicarFilaSerapBoletim;
+using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.ConsolidacaoBoletimProvaAluno
+{
+    public class ExecutarConsolidacaoBoletimProvaAlunoUseCase : AbstractUseCase,
+        IExecutarConsolidacaoBoletimProvaAlunoUseCase
+    {
+        public ExecutarConsolidacaoBoletimProvaAlunoUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task Executar()
+        {
+            SentrySdk.AddBreadcrumb($"Mensagem ExecutarConsolidacaoBoletimProvaAlunoUseCase", "Rabbit - ExecutarConsolidacaoBoletimProvaAlunoUseCase");
+
+            await mediator.Send(new PublicarFilaSerapBoletimCommand(RotasRabbitSerapBoletim.BuscarProvasFinalizadas, string.Empty));
+        }
+    }
+}
---
+++ src/SME.Worker.Agendador.Aplicacao/Comandos/PublicarFilaSerapBoletim/PublicarFilaSerapBoletimCommand.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace SME.Worker.Agendador.Aplicacao.Comandos.PublicarFilaSerapBoletim
+{
+    public class PublicarFilaSerapBoletimCommand : IRequest<bool>
+    {
+        public PublicarFilaSerapBoletimCommand(string fila, object mensagem)
+        {
+            Fila = fila;
+            Mensagem = mensagem;
+        }
+
+        public string Fila { get; set; }
+        public object Mensagem { get; set; }
+    }
+
+    public class PublicarFilaSerapBoletimCommandValidator : AbstractValidator<PublicarFilaSerapBoletimCommand>
+    {
+        public PublicarFilaSerapBoletimCommandValidator()
+        {
+            RuleFor(c => c.Fila)
+               .NotEmpty()
+               .WithMessage("O nome da fila deve ser informado para publicar na fila do Serap boletim.");
+
+            RuleFor(c => c.Mensagem)
+               .NotEmpty()
+               .WithMessage("O objeto da mensagem ser informado para publicar na fila do Serap boletim.");
+        }
+    }
+}
