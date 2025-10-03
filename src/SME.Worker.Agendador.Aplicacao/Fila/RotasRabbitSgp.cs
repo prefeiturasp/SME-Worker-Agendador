ffa10274dda11173223c6517cc5d3c53c2f228bf
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-03 09:40:58 -0300

    Ajustar nome da fila

--- src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSgp.cs
+++ src/SME.Worker.Agendador.Aplicacao/Fila/RotasRabbitSgp.cs
@@ -74,7 +74,7 @@
         public const string ConsolidarInformacoesProdutividadeFrequencia = "sgp.consolidacao.produtividade.frequencia";
         public const string ConsolidarInformacoesFrequenciaPainelEducacional = "sgp.consolidacao.frequencia.analise.painel.educacional";
         public const string ConsolidarIdepPainelEducacional = "sgp.consolidacao.idep.painel.educacional";
-        public const string ConsolidarSondagemEscritaUePainelEducacional = "sgp.consolidacao.painel.educacional.sondagem.escrita.ue";
+        public const string ConsolidarSondagemEscritaUePainelEducacional = "sgp.consolidacao.painel.educacional.sondagem-escrita-ue";
         public const string ConsolidarVisaoGeralPainelEducacional = "sgp.consolidacao.visao.geral.painel.educacional";
         public const string ConsolidarNivelEscritaAlfabetizacao = "sgp.consolidar.nivel.escrita.alfabetizacao";
         public const string ConsolidarNivelEscritaAlfabetizacaoCritico = "sgp.consolidar.nivel.escrita.alfabetizacao.critico";
