54d00578c26eb310d1287edd2006a793896caa4b
Mônica Chiesa <monica.chiesa@universo.univates.br>
2025-10-17 16:19:52 -0300

    Criar job no agendador para ser executado diariamente após as 4 horas da manhã

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/PainelEducacional/ConsolidarReclassificacaoPainelEducacionalUseCase.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Sentry;
+using SME.Worker.Agendador.Aplicacao.Comandos;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.PainelEducacional
+{
+    public class ConsolidarReclassificacaoPainelEducacionalUseCase : AbstractUseCase, IConsolidarReclassificacaoPainelEducacionalUseCase
+    {
+        public ConsolidarReclassificacaoPainelEducacionalUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task Executar()
+        {
+            SentrySdk.AddBreadcrumb($"Mensagem ConsolidarReclassificacaoPainelEducacionalUseCase", "Rabbit - ConsolidarReclassificacaoPainelEducacionalUseCase");
+
+            await mediator.Send(new PublicaFilaRabbitCommand(RotasRabbitSgp.ConsolidarReclassificacaoPainelEducacional, Guid.NewGuid()));
+        }
+    }
+}
--- src/SME.Worker.Agendador.Background/RegistraServicosRecorrentes.cs
+++ src/SME.Worker.Agendador.Background/RegistraServicosRecorrentes.cs
@@ -250,6 +250,9 @@ namespace SME.Worker.Agendador.Background
             // Executar rotina uma vez ao dia, às 02:20am
             Cliente.ExecutarPeriodicamente<IConsolidarSondagemEscritaUePainelEducacionalUseCase>(c => c.Executar(), Cron.Daily(5, 20));
 
+            // Executar rotina uma vez ao dia, às 04:30am
+            Cliente.ExecutarPeriodicamente<IConsolidarReclassificacaoPainelEducacionalUseCase>(c => c.Executar(), Cron.Daily(7, 30));
+
         }
         public static void RegistrarServicosConectaFormacao()
         {
