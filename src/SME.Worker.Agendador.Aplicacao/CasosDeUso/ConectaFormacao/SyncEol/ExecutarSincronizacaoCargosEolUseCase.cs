132ba5c200b40ab8760d53edd45e782d322936ff
Diego Moreno <dierudito@gmail.com>
2025-12-03 11:39:03 -0300

    Adiciona agenda para sync EOL para ConectaFormacao
    AB#138637
    AB#139197

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/ConectaFormacao/SyncEol/ExecutarSincronizacaoCargosEolUseCase.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using SME.Worker.Agendador.Aplicacao.Comandos;
+using SME.Worker.Agendador.Aplicacao.Fila;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.ConectaFormacao.SyncEol
+{
+    public class ExecutarSincronizacaoCargosEolUseCase : AbstractUseCase, IExecutarSincronizacaoCargosEolUseCase
+    {
+        public ExecutarSincronizacaoCargosEolUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+        public async Task<bool> Executar()
+        {
+            await mediator.Send(new PublicaFilaRabbitCommand(RotasConectaFormacao.SincronizaCargosEol, Guid.NewGuid()));
+            return true;
+        }
+    }
+}
---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/ConectaFormacao/SyncEol/SincronizarFuncaoAtividadeEolUseCase.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SME.Worker.Agendador.Aplicacao.Comandos;
+using SME.Worker.Agendador.Aplicacao.Fila;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.ConectaFormacao.SyncEol
+{
+    public class SincronizarFuncaoAtividadeEolUseCase : AbstractUseCase, ISincronizarFuncaoAtividadeEolUseCase
+    {
+        public SincronizarFuncaoAtividadeEolUseCase(IMediator mediator) : base(mediator)
+        {
+        }
+
+        public async Task<bool> Executar()
+        {
+            await mediator.Send(new PublicaFilaRabbitCommand(RotasConectaFormacao.SincronizaFuncaoAtividade, Guid.NewGuid()));
+            return true;
+        }
+    }
+}
