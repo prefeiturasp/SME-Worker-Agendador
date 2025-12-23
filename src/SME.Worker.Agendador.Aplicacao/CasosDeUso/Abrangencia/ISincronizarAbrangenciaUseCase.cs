8e60dd5e5afcecf2938b97bb2a4a8ca43d17c9fe
CiceroHenriqueSantos <50219791+CiceroHenriqueSantos@users.noreply.github.com>
2025-12-23 10:27:08 -0300

    Sincronizar abrangencia

---
+++ src/SME.Worker.Agendador.Aplicacao/CasosDeUso/Abrangencia/ISincronizarAbrangenciaUseCase.cs
@@ -0,0 +1,8 @@
+﻿using SME.Worker.Agendador.Infra;
+
+namespace SME.Worker.Agendador.Aplicacao.CasosDeUso.Abrangencia
+{
+    public interface ISincronizarAbrangenciaUseCase : IUseCaseAgendador
+    {
+    }
+}
